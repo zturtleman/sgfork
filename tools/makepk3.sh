b1d8b372c8cbf6df525d9f248461d01a9758ea29
igorpanarin <igorpanarin@72feb0d0-8cb4-11de-a499-7ffba83fc0c5>
2009-08-29 19:24:19 +0000

    makepk3.sh update for Win platform

--- tools/makepk3.sh
+++ tools/makepk3.sh
@@ -1,37 +1,63 @@
 #!/bin/sh
 
 PLATFORM=`uname | sed -e 's/_.*//' | tr '[:upper:]' '[:lower:]'`
-VMPAKNAME="sgfork-vms.pk3"
+VMPAKNAME="sgfork-qvms.pk3"
 CONFPAKNAME="sgfork-configs.pk3"
+UIPAKNAME="sgfork-ui.pk3"
+GFXPAKNAME="sgfork-gfx.pk3"
 CONFDIR="configs"
 PWD=`pwd`
-PREF=
+MAINBINARY=
+DEDIBINARY=
 
 if [ "$PLATFORM" = "mingw32" ]
 then
   BDIR=build/release-mingw32-x86
-  HDIR=
+  if [ "$1" = "-home" ]
+  then
+    shift
+    HDIR=$1
+    shift
+  else
+    HDIR=$PWD
+  fi
+  MAINBINARY=smokinguns.x86.exe
+  DEDIBINARY=smokinguns_dedicated.x86.exe
 fi
 
 if [ "$PLATFORM" = "linux" ]
 then
   BDIR=build/release-linux-i386
   HDIR=$HOME/.smokinguns/base
+  MAINBINARY=smokinguns.x86.exe
+  DEDIBINARY=smokinguns_dedicated.x86.exe
 fi
 
 if [ `basename $PWD` = "tools" ]
 then
-  PREF=..
-else
-  PREF=.
+  cd ../
 fi
 
 make clean
-make BUILD_CLIENT=0 BUILD_CLIENT_SMP=0 BUILD_SERVER=0 BUILD_GAME_SO=0 BUILD_GAME_QVM=1 $*
+if [ "$1" = "-buildall" ]
+then
+  shift
+  make
+  cd $BDIR
+  mv $MAINBINARY "$HDIR/../"
+  mv $DEDIBINARY "$HDIR/../"
+  cd ../../
+else
+  make BUILD_CLIENT=0 BUILD_CLIENT_SMP=0 BUILD_SERVER=0 BUILD_GAME_SO=0 BUILD_GAME_QVM=1 $*
+fi
 
-cd $PREF/$BDIR/smokinguns/
+cd $BDIR/smokinguns/
 zip -r $VMPAKNAME vm/
-mv $VMPAKNAME $HDIR
-#cd $PWD
+mv $VMPAKNAME "$HDIR"
 cd ../../..
-
+cd base
+zip -r $UIPAKNAME ui/
+zip -r $GFXPAKNAME gfx/
+mv $UIPAKNAME $GFXPAKNAME "$HDIR"
+cd ../
+cd $PWD
